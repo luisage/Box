df5b8a4532aa7ffd3a7b286bd0e54ae5dfe27b1a
Luis Gomez <luis.gomez@kodevox.com>
2026-03-25 18:37:46 -0600

    cambio en prisma para deploy

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../app/generated/prisma/client'
+import { PrismaClient } from '../generated/prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
