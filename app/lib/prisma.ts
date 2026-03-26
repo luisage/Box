e280f5ce300cd0dda791a1849fabc6aee61f1a48
Luis Gomez <luis.gomez@kodevox.com>
2026-03-26 10:34:51 -0600

    Cambio en package_2

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,12 +1,3 @@
-/*import { PrismaClient } from '@/app/generated/prisma'
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
-export const prisma = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-*/
-
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
